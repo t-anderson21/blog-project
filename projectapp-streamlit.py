949e48b52c24eb9ad03e57cf877c46c736e03b30
“Talmage <talmage.anderson17@com>
2024-04-11 16:02:39 -0600

    setting up app

--- projectapp-streamlit.py
+++ projectapp-streamlit.py
@@ -2,3 +2,10 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.title('Economic Indicators Trends')
+st.caption("Data Project for my Stat 386 class")
+st.divider()
+
+url = 'https://github.com/esnt/Data/raw/main/Names/popular_names.csv' # update this...
+df = pd.read_csv(url)
+full_df = pd.read_csv("full_data.csv")

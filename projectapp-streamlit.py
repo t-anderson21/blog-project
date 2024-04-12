f2e515c8f84d2ef7240251491a98b6da65c1dbd3
“Talmage <talmage.anderson17@com>
2024-04-12 09:07:52 -0600

    trying to code app

--- projectapp-streamlit.py
+++ projectapp-streamlit.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-# Set the theme to "light"
-st.set_theme('light')
+
+base="dark"
+primaryColor="purple"
 
 st.title('Economic Indicator Trends')
 st.caption("Data Project for my Stat 386 class")

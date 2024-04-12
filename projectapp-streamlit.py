3880fb4908e878ee76c802081ecf50442882ea08
“Talmage <talmage.anderson17@com>
2024-04-11 20:28:07 -0600

    upload streamlit

--- projectapp-streamlit.py
+++ projectapp-streamlit.py
@@ -6,6 +6,10 @@ st.title('Economic Indicators Trends')
 st.caption("Data Project for my Stat 386 class")
 st.divider()
 
-url = 'https://github.com/esnt/Data/raw/main/Names/popular_names.csv' # update this...
+url = 'https://github.com/t-anderson21/blog-project/blob/main/full_data.csv' # update this...
 df = pd.read_csv(url)
 full_df = pd.read_csv("full_data.csv")
+
+df.head()
+
+full_df.head()

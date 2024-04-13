58ef3e94a2b755c80fa3b7f5839b4283233509e5
“Talmage <talmage.anderson17@com>
2024-04-12 19:41:43 -0600

    adding pages

--- projectapp-streamlit.py
+++ projectapp-streamlit.py
@@ -5,21 +5,28 @@ import seaborn as sns
 import streamlit as st
 
 st.title('Economic Indicator Trends')
-st.caption("Data Project for my Stat 386 class")
+st.caption("App Creation for my Stat 386 class")
 st.divider()
+
+st.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page 🎈")
 
 st.markdown("# Page 2 ❄️")
 st.sidebar.markdown("# Page 2 ❄️")
 
+st.markdown("# Page 3 🎉")
+st.sidebar.markdown("# Page 3 🎉")
+
 #url = 'https://github.com/t-anderson21/blog-project/blob/main/full_data.csv' # update this...
 #df = pd.read_csv(url)
 full_df = pd.read_csv("full_data.csv")
-
 full_df['Date'] = pd.to_datetime(full_df['Date'])
 
 # filtered data for after 2019
 filtered_df = full_df[(full_df['Date'].dt.year >= 2019)]
 
+
+# add header of dataset on one of the pages??
 print(full_df.head())
 
 st.divider()

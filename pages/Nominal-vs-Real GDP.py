724a530f264fd0a9a7802f521ecec1e3391fb2df
“Talmage <talmage.anderson17@com>
2024-04-15 19:30:36 -0600

    adding plots

--- pages/Nominal-vs-Real GDP.py
+++ pages/Nominal-vs-Real GDP.py
@@ -45,3 +45,37 @@ plt.grid(True)
 
 # Display the plot
 st.pyplot(plt)
+
+st.header("Comparison of the Unemployment Rate and Civilian Labor Force Participation Rate")
+st.write("🎈______________🎈")
+
+# Allow the user to select the duration of data to visualize
+selected_duration = st.selectbox('Select duration', ['Last year', 'Last 5 years', 'Last 10 years', 'Last 20 years'])
+
+# Calculate start date based on selected duration
+end_date = datetime.now()
+if selected_duration == 'Last year':
+    start_date = end_date - timedelta(days=365)
+elif selected_duration == 'Last 5 years':
+    start_date = end_date - timedelta(days=365*5)
+elif selected_duration == 'Last 10 years':
+    start_date = end_date - timedelta(days=365*10)
+elif selected_duration == 'Last 20 years':
+    start_date = end_date - timedelta(days=365*20)
+
+# Filter DataFrame based on selected duration
+selected_data = full_df[(full_df['Date'] >= start_date) & (full_df['Date'] <= end_date)]
+
+# Plot Unemployment and CIVPART
+plt.figure(figsize=(10, 6))
+plt.plot(selected_data['Date'], selected_data['Unemployment'], label='Unemployment', marker='o')
+plt.plot(selected_data['Date'], selected_data['CIVPART'], label='CIVPART', marker='o')
+plt.xlabel('Date')
+plt.ylabel('Value')
+plt.title(f'Unemployment vs CIVPART ({selected_duration})')
+plt.legend()
+plt.xticks(rotation=45)
+plt.grid(True)
+
+# Display the plot
+st.pyplot(plt)
---
+++ pages/Pandemic-Trends.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from datetime import datetime, timedelta
+
+st.title(" Trends during the COVID-19 pandemic")
+
+
+
+
+st.head("Future Research on Predictors of Recessions....")
